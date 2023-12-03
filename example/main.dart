7c0c3f1dc96362d9d26eb71f7b4b5d1a306360a6
mcmah309 <dillonmcmahon79@gmail.com>
2023-12-02 22:30:41 -0500

    doc: Add example

---
+++ example/main.dart
@@ -0,0 +1,22 @@
+import 'package:rust_core/result.dart';
+import 'package:tapper/tapper.dart';
+
+void main() {
+  int? number = 10;
+  number = number.tap((n) => ++n).tap((n) => print("The number is $n"));
+  // Prints: The number is 10
+
+  number.pipe((n) => ++n).pipe((n) {
+    print("The number is $n");
+    return n;
+  });
+  // Prints: The number is 11
+
+  String numericString = "123";
+  number = numericString.convInt(); // convInt exists for this type
+  // number is now 123
+
+  String nonNumericString = "abc";
+  Result<int, ConvException> result = nonNumericString.tryConv<int>();
+  // conversion is not possible and handled with Result
+}

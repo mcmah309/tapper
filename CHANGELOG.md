ca168d016e2cff3ad668eb84f4a0efda6925bbb1
mcmah309 <dillonmcmahon79@gmail.com>
2024-01-29 00:20:12 +0000

    chore: Bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.7
+
+- Try Conv for Iterable
+
 # 0.1.6
 
 - Update dependency
--- test/conv_test.dart
+++ test/conv_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:tapper/src/conv.dart';
 import 'package:test/test.dart';
 

a9e7666e0a7e3083134980de066636d667a9e99f
mcmah309 <dillonmcmahon79@gmail.com>
2024-10-08 14:40:33 +0000

    feat: Remove unneeded zip extensions

--- lib/src/zip.dart
+++ lib/src/zip.dart
@@ -15,16 +15,6 @@ extension ZipExtension1A<A extends Object> on (A,) {
   }
 }
 
-extension ZipExtension1B<A extends Object> on (A?,) {
-  @pragma("vm:prefer-inline")
-  (A, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, other);
-  }
-}
-
-//************************************************************************//
 
 extension ZipExtension2A<A extends Object, B extends Object> on (A, B) {
   @pragma("vm:prefer-inline")
@@ -34,35 +24,6 @@ extension ZipExtension2A<A extends Object, B extends Object> on (A, B) {
   }
 }
 
-extension ZipExtension2B<A extends Object, B extends Object> on (A?, B) {
-  @pragma("vm:prefer-inline")
-  (A, B, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, this.$2, other);
-  }
-}
-
-extension ZipExtension2C<A extends Object, B extends Object> on (A, B?) {
-  @pragma("vm:prefer-inline")
-  (A, B, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$2 == null) return null;
-    if (other == null) return null;
-    return (this.$1, this.$2!, other);
-  }
-}
-
-extension ZipExtension2D<A extends Object, B extends Object> on (A?, B?) {
-  @pragma("vm:prefer-inline")
-  (A, B, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (this.$2 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, this.$2!, other);
-  }
-}
-
-//************************************************************************//
 
 extension ZipExtension3A<A extends Object, B extends Object, C extends Object>
     on (A, B, C) {
@@ -73,77 +34,4 @@ extension ZipExtension3A<A extends Object, B extends Object, C extends Object>
   }
 }
 
-extension ZipExtension3B<A extends Object, B extends Object, C extends Object>
-    on (A?, B, C) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, this.$2, this.$3, other);
-  }
-}
 
-extension ZipExtension3C<A extends Object, B extends Object, C extends Object>
-    on (A, B?, C) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$2 == null) return null;
-    if (other == null) return null;
-    return (this.$1, this.$2!, this.$3, other);
-  }
-}
-
-extension ZipExtension3D<A extends Object, B extends Object, C extends Object>
-    on (A, B, C?) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$3 == null) return null;
-    if (other == null) return null;
-    return (this.$1, this.$2, this.$3!, other);
-  }
-}
-
-extension ZipExtension3E<A extends Object, B extends Object, C extends Object>
-    on (A?, B?, C) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (this.$2 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, this.$2!, this.$3, other);
-  }
-}
-
-extension ZipExtension3F<A extends Object, B extends Object, C extends Object>
-    on (A?, B, C?) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (this.$3 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, this.$2, this.$3!, other);
-  }
-}
-
-extension ZipExtension3G<A extends Object, B extends Object, C extends Object>
-    on (A, B?, C?) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$2 == null) return null;
-    if (this.$3 == null) return null;
-    if (other == null) return null;
-    return (this.$1, this.$2!, this.$3!, other);
-  }
-}
-
-extension ZipExtension3H<A extends Object, B extends Object, C extends Object>
-    on (A?, B?, C?) {
-  @pragma("vm:prefer-inline")
-  (A, B, C, Z)? zip<Z extends Object>(Z? other) {
-    if (this.$1 == null) return null;
-    if (this.$2 == null) return null;
-    if (this.$3 == null) return null;
-    if (other == null) return null;
-    return (this.$1!, this.$2!, this.$3!, other);
-  }
-}
--- test/zip_test.dart
+++ test/zip_test.dart
@@ -25,14 +25,6 @@ void main() {
   test('zip1', () {
     (int, int)? x = (provided(),).zip(missing());
     expect(x, null);
-    x = (missing(),).zip(provided());
-    expect(x, null);
-    x = (1,).zip(missing());
-    expect(x, null);
-    x = (missing(),).zip(1);
-    expect(x, null);
-    x = (missing(),).zip(missing());
-    expect(x, null);
     x = (provided(),).zip(provided());
     expect(x, (1, 1));
     x = (1,).zip(2);
@@ -44,21 +36,9 @@ void main() {
 
     x = (provided(), provided()).zip(missing());
     expect(x, null);
-    x = (provided(), missing()).zip(missing());
-    expect(x, null);
-    x = (missing(), provided()).zip(missing());
-    expect(x, null);
-    x = (missing(), missing()).zip(missing());
-    expect(x, null);
 
     x = (provided(), provided()).zip(provided());
     expect(x, (1, 1, 1));
-    x = (provided(), missing()).zip(provided());
-    expect(x, null);
-    x = (missing(), provided()).zip(provided());
-    expect(x, null);
-    x = (missing(), missing()).zip(provided());
-    expect(x, null);
   });
 
   test('zip3', () {
@@ -66,35 +46,8 @@ void main() {
 
     x = (provided(), provided(), provided()).zip(missing());
     expect(x, null);
-    x = (provided(), provided(), missing()).zip(missing());
-    expect(x, null);
-    x = (provided(), missing(), provided()).zip(missing());
-    expect(x, null);
-    x = (missing(), provided(), provided()).zip(missing());
-    expect(x, null);
-    x = (missing(), provided(), missing()).zip(missing());
-    expect(x, null);
-    x = (missing(), missing(), provided()).zip(missing());
-    expect(x, null);
-    x = (provided(), missing(), missing()).zip(missing());
-    expect(x, null);
-    x = (missing(), missing(), missing()).zip(missing());
-    expect(x, null);
 
     x = (provided(), provided(), provided()).zip(provided());
     expect(x, (1, 1, 1, 1));
-    x = (provided(), provided(), missing()).zip(provided());
-    expect(x, null);
-    x = (provided(), missing(), provided()).zip(provided());
-    expect(x, null);
-    x = (missing(), provided(), provided()).zip(provided());
-    expect(x, null);
-    x = (missing(), provided(), missing()).zip(provided());
-    expect(x, null);
-    x = (missing(), missing(), provided()).zip(provided());
-    expect(x, null);
-    x = (provided(), missing(), missing()).zip(provided());
-    expect(x, null);
-    x = (missing(), missing(), missing()).zip(provided());
   });
 }
